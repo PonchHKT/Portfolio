324344ceaf63d922e4a4c1835b6830c1b8340469
Pornchai Chanaloet <kayziopro@gmail.com>
2021-12-22 21:04:29 +0100

    CSS, EmailJS, PopupMsg /contact

---
+++ components/MessagePopup.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../styles/contact.module.scss'
+import Link from 'next/link'
+
+function Popup(props) {
+    return (props.trigger) ? (
+        <div className={styles.popup}>
+        <div className={styles.popupinner}>
+        <div className={styles.popupitems}>
+        <Link href="/">
+        <i class="fad fa-times-circle"></i>
+        </Link>
+        { props.children }
+        </div>
+        </div>
+        </div>
+     )   : "";
+}
+export default Popup
--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,26 @@
 import styles from '../styles/contact.module.scss'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import emailjs from 'emailjs-com';
+import Popup from '../components/MessagePopup'
+import { useState } from 'react';
 
 export default function Contact() {
+
+    const [buttonPopup, setButtonPopup] = useState(false);
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+    emailjs.send('service_bmp205s','template_q9dercq', e.target.value, 'user_70g4V65QW8zpZyreiScG8')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+        e.target.reset()
+    }
+
     return (
         
         
@@ -14,35 +32,49 @@ export default function Contact() {
 
                     <div className={styles.hello}>
                         <h1>Hello !</h1>
-                        <h2>I am actually writing anything about anything because it distract you from knowing that this is just a Loremipsum but way better.</h2>
+                        <h2>Vous pouvez me contacter à travers les réseaux sociaux présentés ici ou sinon <br/>n’hésitez pas de me laisser un message en utilisant le formulaire ci-dessous.</h2>
                     </div>
+
+                    <div className={styles.social}>
+                            <ul>
+                                <i class="fab fa-facebook-square" style={{color: '#4267B2'}}></i>
+                                <i class="fab fa-linkedin" style={{color: '#0077b5'}}></i>
+                                <i class="fab fa-discord" style={{color: '#7289da'}}></i>
+                                <i class="fab fa-instagram" style={{color: '#8a3ab9'}}></i>
+                            </ul>
+                        </div>
 
                     <div className={styles.contactboxContainer}>
 
                         <div className={styles.contactBox}>
 
+                            <form onSubmit={sendEmail}>
                             <div className={styles.numberforms}>
                             <h3>Numéro</h3>
-                            <input className={styles.inputnumber} placeholder="Numéro de téléphone"/>
+                            <input name="number" type="number" className={styles.inputnumber} placeholder="Numéro de téléphone" minLength="9"/>
                             </div>
 
                             <div className={styles.emailforms}>
                             <h3>E-mail</h3>
-                            <input className={styles.inputemail} placeholder="Votre adresse électronique"/>
+                            <input name="email" type="email" className={styles.inputemail} placeholder="Votre adresse électronique" minLength="5"/>
                             </div>
 
                             
                             <div className={styles.msgforms}>
                             <h3>Message</h3>
-                            <input className={styles.inputmsg} placeholder="Bonjour"/>
+                            <input name="message" className={styles.inputmsg} placeholder="Bonjour" minLength="10"/>
                             </div>
 
                             <div className={styles.buttonContainer}>
-                            <button>Envoyer</button>
+                            <button onClick={() => setButtonPopup(true)} type="submit" value="Send Message">Envoyer</button>
                             </div>
+                            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                                <h3>Votre message a bien été envoyé !</h3>
+                                <p>Je vous répondrais dans les meilleurs délais.</p>
+                            </Popup>
+                        </form>
 
                         </div>
-
 
             </div>
 
