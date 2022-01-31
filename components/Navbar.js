406732698726e47b426cbe4261f2d5bad43d6b04
Pornchai Chanaloet <kayziopro@gmail.com>
2022-01-31 01:00:22 +0100

    Update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from '../styles/nav.module.scss'
 
 function Navbar() {
+
+    const [displayMenu, setDisplayMenu] = useState(false)
+
     return (
         <header className={styles.navContainer}>
             <div className={styles.navItems}>
@@ -10,12 +14,20 @@ function Navbar() {
                     <p>ShuxDev.</p>
                 </div>
 
-                <ul>
+                <ul className={`${displayMenu ? styles.wNavbarActive : ''} ${styles.wNavbar}`}>
                     <li><Link href="/">Accueil</Link></li>
+                    <hr/>
                     <li><Link href="/about">A propos de moi</Link></li>
+                    <hr/>
                     <li><Link href="/project">Projets</Link></li>
+                    <hr/>
                     <li><Link href="/contact">Me contacter</Link></li>
                 </ul>
+
+                <div onClick={() => setDisplayMenu(!displayMenu)} className={`${displayMenu ? styles.wNavbar2Active : ''} ${styles.wNavbar2}`}></div>
+
+                <i onClick={() => setDisplayMenu(!displayMenu)} class="fad fa-bars"></i>
+
 
             </div>
 
