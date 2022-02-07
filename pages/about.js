3f10bc319905d2df486513b6dbe13ce3f68d1cc1
Pornchai Chanaloet <kayziopro@gmail.com>
2022-02-07 18:57:39 +0100

    Update

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/about.module.scss'
 import Hability from '../components/Hability'
+import Link from 'next/link';
+import PurpleButton from '../components/PurpleButton'
 
 export default function About() {
 
@@ -16,14 +18,45 @@ export default function About() {
 
                     <div className={styles.aboutBox}>
 
-                        <img src="https://cdn.discordapp.com/attachments/789414641498325032/920306693823430696/image0.png"/>
+                    <div className={styles.imgBox}>
+                        <img src="http://cdn26.us1.fansshare.com/photo/purplewallpapers/purple-wallpaper-purple-wallpapers-476630079.jpg" ></img>
+                        <div className={styles.divImg} style={{width: "426px", height: "240px"}}></div>
+                    </div>
 
-                        <div className={styles.aboutText}>
-                            <h1>Pornchai Chanaloet</h1>
-                            <h2>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</h2>
-                        
+                    <div className={styles.aboutCatContainer}>
+
+                        <div className={styles.aboutCategory}>
+
+                            <button>Bio</button>
+                            <button>Formation</button>
+                            <button>Éxpériences</button>
 
                         </div>
+
+                        <p className={styles.aboutText}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</p>
+
+                        <div className={styles.aboutInfos}>
+
+                            <div>
+                            <p><strong>Age :</strong> 21 ans</p>
+                            <p><strong>Freelance :</strong> Disponible</p>
+                            </div>
+
+                            <div>
+                            <p><strong>Nationalité :</strong> FR / TH</p>
+                            <p><strong>Email :</strong> p.chanaloet@efrei.net</p>
+                            </div>
+
+                            <div>
+                            <Link href="/about">
+                                <button className={styles.contactMe} icon="fad fa-hand-point-right">Me contacter</button>
+                            </Link>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    
 
                     </div>
 
