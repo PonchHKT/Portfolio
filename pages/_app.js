dec233787a1201cff16fed32d9e23fb2745a8fd9
Pornchai Chanaloet <kayziopro@gmail.com>
2022-02-08 17:38:57 +0100

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }) {
                 <title>Ponch - Portfolio</title>
                 <meta name="description" content="Bienvenue sur mon Portfolio !" />
                 <meta name="keywords" content="ponch, pornchai, pornchai chanaloet, shux, shuxdev, ponchhkt, " />
-                <meta property="og:image" content="%PUBLIC_URL%/img/Capture.PNG" />
-                <meta name="twitter:image" content="%PUBLIC_URL%/img/Capture.PNG" />
+                <meta property="og:image" content="https://shuxdev.vercel.app/img/Capture.PNG" />
+                <meta name="twitter:image" content="https://shuxdev.vercel.app/img/Capture.PNG" />
                 <link rel="icon" href="./img/Capture.PNG" />
             </Head>
             <Component {...pageProps} />
